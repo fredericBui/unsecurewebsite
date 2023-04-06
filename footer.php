8743e54f952d8f770f632b20ca740329e147ed4a
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-04-06 19:15:22 +0200

    align footer content

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav m-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="/unsecurewebsite/pages/mentions-legales.php">Mentions légales</a>
                 </li>
